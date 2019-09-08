bbbb1aeb517236ac24a46fbb4aeed6ed4d312be0
hiddenWarrior <manfscience@gmail.com>
2019-09-08 20:20:59 +0200

    making bootstrap with spring dependency injection. Closes #22

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -3,7 +3,10 @@ package guru.springframework.sfgpetclinic.services.map;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.CrudService;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService  {
     @Override
     public Owner save(Owner obj) {
